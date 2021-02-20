b452937c25ac273c3ebbcb064e5bbadcfc523265
Thomas Tresgots <ttresgots@gmail.com>
2021-02-20 20:49:30 +0100

    constructor in Choice

--- src/com/company/Choice.java
+++ src/com/company/Choice.java
@@ -1,14 +1,46 @@
 package com.company;
 
-import java.util.ArrayList;
-
 public class Choice {
     String description;
-    ArrayList<Faction> factions;
+    int[] satisfactionReductions;
     int treasury;
     int food;
     int industry;
-    int farms;
+    int farm;
+
+    public Choice(String description, int[] satisfactionReductions, int treasury, int food, int industry, int farm) {
+        this.description = description;
+        this.satisfactionReductions = satisfactionReductions;
+        this.treasury = treasury;
+        this.food = food;
+        this.industry = industry;
+        this.farm = farm;
+    }
+
+    public Choice fromText(String csvLine){
+        String[] everything = csvLine.split(";");
+
+        String description = everything[0];
+        String[] satisfactionReductions = everything[1].split(",");
+        int[] satisfactionsReductionsConverted = new int[8];
+
+        int i = 0;
+        for (String oneInt: satisfactionReductions) {
+            satisfactionsReductionsConverted[i] = Integer.parseInt(oneInt);
+            i ++;
+        }
+
+
+
+        int treasury = Integer.parseInt(everything[2]);
+        int food= Integer.parseInt(everything[3]);
+        int industry = Integer.parseInt(everything[4]);
+        int farm = Integer.parseInt(everything[5]);
+
+        return new Choice(description, satisfactionsReductionsConverted, treasury , food , industry , farm);
+
+    }
+
 
     //TODO constructor from file/text/csv whatever
 
---
+++ test/company/PartyTest.java
@@ -0,0 +1,12 @@
+package company;
+
+import com.company.Country;
+import com.company.Party;
+import org.junit.Test;
+
+public class PartyTest {
+
+    @Test
+    public void Should_NotEnd_When_NotAllCountryAreDown(){
+    }
+}
