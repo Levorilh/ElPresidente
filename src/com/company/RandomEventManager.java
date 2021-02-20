0a33bb1b8725be5187d0edfa6d715070e32496a0
Horville <59647345+Horville@users.noreply.github.com>
2021-02-20 22:31:50 +0100

    Update RandomEventManager.java

--- src/com/company/RandomEventManager.java
+++ src/com/company/RandomEventManager.java
@@ -3,11 +3,25 @@ package com.company;
 import java.util.ArrayList;
 
 public class RandomEventManager {
-    
+    Season season;
+    ArrayList<Circumstance> listCircumstance;
+
+
+
     public RandomEventManager(ArrayList<Circumstance> listCircumstance, Season season) {
+        this.listCircumstance=listCircumstance;
+        this.season=season;
     }
 
     public Event getEvent() {
-        //TODO: Choisi un Event en fonction du scenario/saison(Party.Round % 4)
+
+    }
+
+    private ArrayList<Event> getEvent(){
+        return new ArrayList<Event>(
+                new Event(new ArrayList<Circumstance>(Circumstance.ANYTIME),Season.MISCELLANEOUS,"Une tornade traverse votre ile !", new ArrayList<Choice>(
+                        new Choice("Utiliser l'étentaille magique pour éloigner la tornade",null,0,0,0,0),
+                        new Choice("Abandonner le femmes et les enfants", new ArrayList<Faction>(new Faction("Loyaliste",-20,-10)),0,0,0,0) ))
+        );
     }
 }
